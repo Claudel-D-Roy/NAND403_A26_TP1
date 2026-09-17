9fb644c650889d5289f0ced71c99080721024832
Claudel <112507354+Claudel-D-Roy@users.noreply.github.com>
2026-09-17 17:36:33 -0400

    added json files

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import json
+import sys
+import PySide6.QtWidgets 
+#import QApplication, QtableWidget, QTableWidgetItem, QMainWindow
+
+
+
+
+
+def Load_json_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
